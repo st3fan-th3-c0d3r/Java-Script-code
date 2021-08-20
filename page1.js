0505c9ddadedb67082c620e364aeb0910ad8ecb3
st3fan-th3-c0d3r <89220671+st3fan-th3-c0d3r@users.noreply.github.com>
2021-08-20 11:14:19 +0300

    Update page1.js

--- page1.js
+++ page1.js
@@ -1,2 +1,52 @@
+
+var temperatures = [17, 25, 21, 34]
 console.log(tmeperatures[1])
 
+
+for (var i = 1; i < 4; i++) {
+
+console.log(i)
+}
+
+
+var temperatures = [17, 25, 21, 34]
+temperatures[2] = 25;
+console.log(temperatures[2])
+
+var t = 20;
+while (t < 24) {
+
+t++;
+console.log(t)
+
+}
+
+var c = 100
+while (c < 120) {
+
+c++
+c++
+console.log(c)
+
+}
+
+
+
+var flourNeeded =  200;
+var flourYouhave = 150 ;
+
+if (flourNeeded > flourYouhave) {
+
+document.write("You still need: " + (flourNeeded - flourYouhave) )
+
+} else if (flourYouhave === flourNeeded) {
+
+    document.write("You have all the flour you need!")
+
+
+}  else if( flourYouhave > flourNeeded) {
+   
+    document.write("You have to get down: " + (flourYouhave - flourNeeded) )
+
+
+} 
